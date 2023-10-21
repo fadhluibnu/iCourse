1859618ee9612f1de68a724535144d7bfe8c03fb
fadhluibnu <f.ibnu.a1@gmail.com>
2023-10-21 09:47:48 +0700

    reply comment migration and fillable table

---
+++ app/Models/ReplyComment.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ReplyComment extends Model
+{
+    use HasFactory, HasUuids;
+
+    protected $fillable = [
+        'id_comment',
+        'name',
+        'author',
+        'body'
+    ];
+}
