b4ffc70dba511de44e17b3b152ca51fa3d712cd0
fadhluibnu <f.ibnu.a1@gmail.com>
2023-10-21 14:30:00 +0700

    migration and model tutorials

---
+++ database/migrations/2023_10_21_072249_create_tutorials_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tutorials', function (Blueprint $table) {
+            $table->uuid('id');
+            $table->string('title');
+            $table->string('slug')->unique();
+            $table->text('description');
+            $table->string('image');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tutorials');
+    }
+};
